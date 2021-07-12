349698fbe9155a6eec4f71a7249fb7edbf86f440
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-07-12 17:53:12 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo-List-App';
+
+ 
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+
+  addItem(description: any) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  deleteEmployee(i : any)
+  {
+    this.allItems.splice(i,1);
+  }
+  
 }
+
